e49c61274e08e98aac000076f74ff903e81e08ba
Carlo Zappacosta <zappyk@gmail.com>
2015-08-07 00:37:21 +0200

    Miglioirie...

--- root/bin/smtp.gmail.com-SSL-CA-Certificate-connect.sh
+++ root/bin/smtp.gmail.com-SSL-CA-Certificate-connect.sh
@@ -1,13 +1,15 @@
 #!/bin/env bash
 
-HOST='smtp.gmail.com'
-PORT=465
+HOST=${1:-smtp.gmail.com}
+PORT=${2:-465}
 
-_CA_connect_FILE="$HOST.crt.pem"
+ _CA_connect_FILE="$HOST.crt.pem"
 
-_CA_connect_Init() { echo -n | openssl s_client -connect $HOST:$PORT | perl -lne "print if /^-----/.../^-----/" >$_CA_connect_FILE; }
+#_CA_connect_Init() { echo -n | openssl s_client -connect $HOST:$PORT | perl -lne "print if /^-----/.../^-----/" >$_CA_connect_FILE; }
+#_CA_connect_Init() { echo -n | openssl s_client -connect $HOST:$PORT            2>/dev/null | openssl x509 -outform PEM; }
+ _CA_connect_Init() {           openssl s_client -connect $HOST:$PORT </dev/null 2>/dev/null | openssl x509 -outform PEM; }
 
-_CA_connect_Done() { openssl s_client -CAfile $_CA_connect_FILE -connect $HOST:$PORT; }
+ _CA_connect_Done() { openssl s_client -CAfile $_CA_connect_FILE -connect $HOST:$PORT; }
 
 _CA_connect_Init && _CA_connect_Done
 
