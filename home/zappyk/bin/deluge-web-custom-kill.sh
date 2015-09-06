ae2713933225a035feaf9dc828200bf619e74527
Carlo Zappacosta <zappyk@gmail.com>
2015-09-06 09:27:01 +0200

    Migliorie...

--- home/zappyk/bin/deluge-web-custom-kill.sh
+++ home/zappyk/bin/deluge-web-custom-kill.sh
@@ -5,7 +5,9 @@ ECN=0
 
 PID_deluge_W=$(pgrep 'deluge-web' | grep -v $PID)
 PID_deluge_D=$(pgrep 'deluged' | grep -v $PID)
-TWONKY_EXEC_="$HOME/Programmi/twonky/twonky.sh stop"
+STOP_twonky_="$HOME/Programmi/twonky/twonky.sh stop"
+
+printf '#%.0s' {1..120} ; echo
 
 [ -n "$PID_deluge_W" ] && PSC_deluge_W=$(ps -efj | grep "$PID_deluge_W " | grep -v grep)
 [ -n "$PID_deluge_D" ] && PSC_deluge_D=$(ps -efj | grep "$PID_deluge_D " | grep -v grep)
@@ -16,6 +18,6 @@ TWONKY_EXEC_="$HOME/Programmi/twonky/twonky.sh stop"
 [ -n "$PID_deluge_W" ] && echo "kill $PID_deluge_W" && ECN=1
 [ -n "$PID_deluge_D" ] && echo "kill $PID_deluge_D" && ECN=1
 
-eval "$TWONKY_EXEC_"
+eval "$STOP_twonky_"
 
 exit $ECN
