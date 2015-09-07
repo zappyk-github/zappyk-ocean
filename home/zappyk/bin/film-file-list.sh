08d241a3105086a82c9e59b260f1269d7885335f
Carlo Zappacosta <zappyk@gmail.com>
2015-09-07 22:26:08 +0200

    Miglioire...

--- home/zappyk/bin/film-file-list.sh
+++ home/zappyk/bin/film-file-list.sh
@@ -3,4 +3,8 @@
 PATH_BASE="$HOME/zappyk-hd/Public/Shared Videos"
 PATH_FILM='Films'
 
-(cd "$PATH_BASE" && film-file-list.pl "$PATH_FILM")
+FILE_SAVE="$HOME/zappyk-hd-Films.csv"
+
+(cd "$PATH_BASE" && film-file-list.pl "$PATH_FILM" >"$FILE_SAVE-" && mv -f "$FILE_SAVE" "$FILE_SAVE~" && mv -f "$FILE_SAVE-" "$FILE_SAVE")
+
+exit
