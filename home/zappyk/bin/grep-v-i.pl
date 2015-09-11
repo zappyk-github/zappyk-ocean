a9f100229aa08724c3db54ab06ea906d6fbdfde4
Carlo Zappacosta <zappyk@gmail.com>
2015-09-11 20:56:15 +0200

    Miglioire...

--- home/zappyk/bin/grep-v-i.pl
+++ home/zappyk/bin/grep-v-i.pl
@@ -17,7 +17,17 @@ sub _isFind {
     my $row = shift;
     my $find = 0;
     foreach my $key (@keys) {
+        $key = _anQuote($key);
         $find = 1 if ($row =~ m/$key/i);
     }
     return($find);
+}
+
+################################################################################
+sub _anQuote {
+    my $string = shift;
+       $string =~ s/\\\(/(/g;
+       $string =~ s/\\\)/)/g;
+       $string =~ s/\\\|/|/g;
+    return($string);
 }
