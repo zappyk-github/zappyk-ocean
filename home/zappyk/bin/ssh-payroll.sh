ad3f46de7e843e7587cce62a28f6d0f9bdb799b7
Carlo Zappacosta <zappyk@gmail.com>
2015-09-19 18:34:12 +0200

    Miglioire...

--- home/zappyk/bin/ssh-payroll.sh
+++ home/zappyk/bin/ssh-payroll.sh
@@ -6,24 +6,26 @@ _COMMAND_=''
 
 SSH_HOST_NAME='sipert'
 SSH_USER_NAME='pes0zap@'
-#--------+------------+
-# HOSTs  | IP ADDRESS |
-#--------+------------+
-# vls    | 10.0.3.5   |
-# pls    | 10.0.3.6   |
-# sls    | 10.0.3.7   |
-# pdb    | 10.0.3.20  |
-# eis    | 10.0.3.21  |
-# test   | 10.0.3.22  |
-# win01  | 10.0.3.23  |
-#--------+------------+
+#-------------+------------+
+# HOSTs       | IP ADDRESS |
+#-------------+------------+
+# vls         | 10.0.3.5   |
+# pls         | 10.0.3.6   |
+# sls         | 10.0.3.7   |
+# pdb         | 10.0.3.20  |
+# eis         | 10.0.3.21  |
+# test        | 10.0.3.22  |
+# win01       | 10.0.3.23  |
+# gestionale  | 10.0.3.99  |
+#-------------+------------+
 
 [ -n "$HOST_NAME" ] && SSH_HOST_NAME=$HOST_NAME
 
 case "$SSH_HOST_NAME" in
-    pls | vls ) SSH_HOST_NAME='10.0.3.5' ;;
-    sipert    ) SSH_HOST_NAME='10.0.3.64' ;;
-    *         ) echo -e "Usage:\n\t$(basename "$0") [ [ USER ] [ HOST ] ]\nHost '$SSH_HOST_NAME' non configurato..." && exit 1
+    pls | vls  ) SSH_HOST_NAME='10.0.3.5' ;;
+    sipert     ) SSH_HOST_NAME='10.0.3.64' ;;
+    gestionale ) SSH_HOST_NAME='10.0.3.99' ;;
+    *          ) echo -e "Usage:\n\t$(basename "$0") [ [ USER ] [ HOST ] ]\nHost '$SSH_HOST_NAME' non configurato..." && exit 1
 esac
 
 [ $(id -u) -eq 0  ] && SSH_USER_NAME='root@'
