08eb07b70e4372ac7ddc9104a2b5cc08de7c1060
Carlo Zappacosta <zappyk@gmail.com>
2015-12-26 19:51:08 +0100

    Miglioire...

--- home/zappyk/bin/svn-admin-repos-remote.sh
+++ home/zappyk/bin/svn-admin-repos-remote.sh
@@ -1,18 +1,23 @@
 #!/bin/env bash
 
-THISHOSTNAME=$(hostname -f)
 REMOTE_HOSTS='zappyk-ws zappyk-mc zappyk-zb zappyk-nb'
+THISHOSTNAME=$(hostname -f)
 
-REMOTE_USERS=$USER
 REMOTE_CMMDS="source .bash_profile ; svn-admin-repos.sh $*"
+REMOTE_USERS=$USER
 
 for remote_host in $REMOTE_HOSTS; do
     [ "$THISHOSTNAME" == "$remote_host" ] && continue
+
     remote_user="$REMOTE_USERS@$remote_host"
     remote_cmmd="ssh $remote_user \"$REMOTE_CMMDS\""
-    echo "${remote_cmmd//?/_}"
-    echo "${remote_cmmd}"
-    eval "${remote_cmmd}" | sed "s/^/$remote_user| /g"
+
+    remote_tags="$remote_user| "
+    remote_leng="$remote_tags$remote_cmmd"
+
+      echo "${remote_leng//?/_}"
+    ( echo "${remote_cmmd}"
+      eval "${remote_cmmd}" ) | sed "s/^/$remote_user| /g"
 done
 
 exit
