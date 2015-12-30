2ee857432b47872354584891e3d2e86281c3be0f
Carlo Zappacosta <zappyk@gmail.com>
2015-12-30 20:31:36 +0100

    Aggiunti script per il controllo remoto (lato user).

---
+++ home/zappyk/bin/ssh-remote-commands.sh
@@ -0,0 +1,30 @@
+#!/bin/env bash
+
+REMOTE_HOSTS='zappyk-ws zappyk-mc zappyk-zb zappyk-nb'
+THISHOSTNAME=$(hostname -f)
+
+LOCALE_PROGR=$1 ; shift
+LOCALE_PARAM=$*
+LOCALE_CMMDS="$LOCALE_PROGR $LOCALE_PARAM"
+REMOTE_CMMDS="source .bash_profile ; $LOCALE_CMMDS"
+REMOTE_USERS=$USER
+
+[ -z "$LOCALE_CMMDS" ] && echo -e "Specifica un comando da eseguire in remoto\n come utente ( $REMOTE_USERS )\n sugli hosts [ $REMOTE_HOSTS ]" && exit 1
+[ -z "$LOCALE_PARAM" ] && echo '...nessun parametro...?' && read
+
+for remote_host in $REMOTE_HOSTS; do
+    remote_user="$REMOTE_USERS@$remote_host"
+    remote_cmmd="ssh $remote_user \"$REMOTE_CMMDS\""
+
+#CZ#[ "$THISHOSTNAME" == "$remote_host" ] && continue
+    [ "$THISHOSTNAME" == "$remote_host" ] && remote_cmmd="$LOCALE_CMMDS"
+
+    remote_tags="$remote_user| "
+    remote_leng="$remote_tags$remote_cmmd"
+
+      echo "${remote_leng//?/_}"
+    ( echo "${remote_cmmd}"
+      eval "${remote_cmmd}" ) | sed "s/^/$remote_user| /g"
+done
+
+exit
--- home/zappyk/bin/svn-admin-repos-remote.sh
+++ home/zappyk/bin/svn-admin-repos-remote.sh
@@ -1,25 +1,5 @@
 #!/bin/env bash
 
-REMOTE_HOSTS='zappyk-ws zappyk-mc zappyk-zb zappyk-nb'
-THISHOSTNAME=$(hostname -f)
-
-LOCALE_CMMDS="svn-admin-repos.sh $*"
-REMOTE_CMMDS="source .bash_profile ; $LOCALE_CMMDS"
-REMOTE_USERS=$USER
-
-for remote_host in $REMOTE_HOSTS; do
-    remote_user="$REMOTE_USERS@$remote_host"
-    remote_cmmd="ssh $remote_user \"$REMOTE_CMMDS\""
-
-#CZ#[ "$THISHOSTNAME" == "$remote_host" ] && continue
-    [ "$THISHOSTNAME" == "$remote_host" ] && remote_cmmd="$LOCALE_CMMDS"
-
-    remote_tags="$remote_user| "
-    remote_leng="$remote_tags$remote_cmmd"
-
-      echo "${remote_leng//?/_}"
-    ( echo "${remote_cmmd}"
-      eval "${remote_cmmd}" ) | sed "s/^/$remote_user| /g"
-done
+ssh-remote-commands.sh 'svn-admin-repos.sh' "$@"
 
 exit
