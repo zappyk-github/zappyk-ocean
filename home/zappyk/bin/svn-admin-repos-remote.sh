719282b8a334795c124449f160c15787eecf01d5
Carlo Zappacosta <zappyk@gmail.com>
2015-12-30 20:08:01 +0100

    Miglioire...

--- home/zappyk/bin/svn-admin-repos-remote.sh
+++ home/zappyk/bin/svn-admin-repos-remote.sh
@@ -3,14 +3,16 @@
 REMOTE_HOSTS='zappyk-ws zappyk-mc zappyk-zb zappyk-nb'
 THISHOSTNAME=$(hostname -f)
 
-REMOTE_CMMDS="source .bash_profile ; svn-admin-repos.sh $*"
+LOCALE_CMMDS="svn-admin-repos.sh $*"
+REMOTE_CMMDS="source .bash_profile ; $LOCALE_CMMDS"
 REMOTE_USERS=$USER
 
 for remote_host in $REMOTE_HOSTS; do
-    [ "$THISHOSTNAME" == "$remote_host" ] && continue
-
     remote_user="$REMOTE_USERS@$remote_host"
     remote_cmmd="ssh $remote_user \"$REMOTE_CMMDS\""
+
+#CZ#[ "$THISHOSTNAME" == "$remote_host" ] && continue
+    [ "$THISHOSTNAME" == "$remote_host" ] && remote_cmmd="$LOCALE_CMMDS"
 
     remote_tags="$remote_user| "
     remote_leng="$remote_tags$remote_cmmd"
