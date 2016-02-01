722ccd7663a6d28b00610483b9c20969ffd74007
Carlo Zappacosta <zappyk@gmail.com>
2016-02-01 23:50:41 +0100

    Miglioire...

--- home/zappyk/bin/pdf2jpg.sh
+++ home/zappyk/bin/pdf2jpg.sh
@@ -5,6 +5,10 @@ file_pdf=$1
 [ ! -n "$file_pdf" ] && echo "Specifica un file PDF da convertire in JPG." && exit 1
 [ ! -e "$file_pdf" ] && echo "Il file '$file_pdf' non esiste o non e' possibile leggerlo." && exit 1
 
-convert -density 300 "$file_pdf" "$file_pdf.jpg"
+path_jpg=$(dirname  "$file_pdf")
+name_jpg=$(basename "$file_pdf" ".pdf")
+file_jpg="$path_jpg/$name_jpg.jpg"
+
+echo convert -density 300 "$file_pdf" "$file_jpg"
 
 exit
