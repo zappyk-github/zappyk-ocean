b16da50a9e6b157191ee9896f7f58d07f2ebd83c
Carlo Zappacosta <zappyk@gmail.com>
2017-05-22 21:14:12 +0200

    Miglioire...

--- home/zappyk/opt/deluge/bin/deluge-watch.sh
+++
@@ -1,8 +0,0 @@
-#command="ls -l incomings* completed* download* torrents*"
- command="ls -d incomings* completed* download* torrents* | xargs"
- command="for dir in \$($command); do echo \"\$dir:\"; du -shc \"\$dir\"/* 2>/dev/null; echo; done; df -hT /"
- command="pwd && echo && $command"
-
-[ "$(uname -n)" != 'raspi.local' ] && dir=${1:-$(dirname $0)} && cd "$dir"
-
-watch -n 5 "$command"
