191322ef0d9c150d05124e309f2085564b3b107f
Carlo Zappacosta <zappyk@gmail.com>
2017-05-27 17:51:59 +0200

    Miglioire...

--- root/bin/peervpn-kill.sh
+++ root/bin/peervpn-kill.sh
@@ -10,11 +10,13 @@ ECN=0
 TAG_peervpn=$TAG
 PID_peervpn=$(pgrep -f "$TAG_peervpn" | grep -v $PID)
 
-printf '#%.0s' {1..120} ; echo
+first=true
 
 for PID_find in $PID_peervpn; do
     PSC_peervpn=$(ps -efj | grep "$PID_find " | grep -v grep)
     if [ -n "$PSC_peervpn" ]; then
+	( $first ) && first=false && \
+        printf '#%.0s' {1..120} && echo
         printf "# %5s = %s\n" "$PID_find" "$PSC_peervpn"
         echo "kill $PID_find"
         ECN=1
