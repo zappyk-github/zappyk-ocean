0d9e9b8d5407d93e1489eeb5772313a0735fff1d
Carlo Zappacosta <zappyk@gmail.com>
2017-06-04 00:25:47 +0200

    Miglioire...

--- home/zappyk/bin/raspberry-temp-check.sh
+++ home/zappyk/bin/raspberry-temp-check.sh
@@ -7,6 +7,7 @@ _max_celsius="49.5"
 
 save_measure=${1:-false}
 name_measure=$(uname -n)
+name_measure=${name_measure%%.*}
 date_measure=$(date +'%Y%m%d %H:%M')
 path_measure="$HOME/log"
 file_measure="$path_measure/$name_measure-$this.csv"
