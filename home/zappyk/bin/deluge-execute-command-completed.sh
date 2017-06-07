3f77fb6f7517872b4e1a8a6eaf56c3f473a974cd
Carlo Zappacosta <zappyk@gmail.com>
2017-06-07 20:35:55 +0200

    Miglioire...

--- home/zappyk/bin/deluge-execute-command-completed.sh
+++ home/zappyk/bin/deluge-execute-command-completed.sh
@@ -68,7 +68,7 @@ _nsm() { ( $test ) && return
     local tdate=$(_now)
     #___________________________________________________________________________
     #
-    local signin="\n\nhost $(uname -n)$(df -h /)"
+    local signin="\n\nhost: $(uname -n) ($(uname -m))\n$(df -H /)"
     local notify="zappyk@gmail.com"
     local string="Completed files are:\n$names\nDone.$signin"
     #___________________________________________________________________________
