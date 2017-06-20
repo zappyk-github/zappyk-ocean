a3271100c731eab7b29a0077b74c1fcf45f3b43f
Carlo Zappacosta <zappyk@gmail.com>
2017-06-20 12:40:31 +0200

    Miglioire...

--- root/bin/peervpn-root-chroot.sh
+++ root/bin/peervpn-root-chroot.sh
@@ -1,23 +1,27 @@
 #!/bin/env bash
 
-chroot_path='/var/empty/peervpn'
+chroot_path='/var/empty/peervpn-1'
+
+[ ! -e "$chroot_path"/bin/ ] && mkdir -pv "$chroot_path"/bin/
+[ ! -e "$chroot_path"/etc/ ] && mkdir -pv "$chroot_path"/etc/
+[ ! -e "$chroot_path"/lib/ ] && mkdir -pv "$chroot_path"/lib/
+[ ! -e "$chroot_path"/usr/ ] && mkdir -pv "$chroot_path"/usr/
+[ ! -e "$chroot_path"/var/ ] && mkdir -pv "$chroot_path"/var/
 
 case $1 in
     -u) ####################################################
-        umount "$chroot_path"/bin/  \
-               "$chroot_path"/etc/  \
-               "$chroot_path"/lib/  \
-               "$chroot_path"/usr/  \
-               "$chroot_path"/sbin/ \
-               "$chroot_path"/var/run/
+        umount "$chroot_path"/bin/ \
+               "$chroot_path"/etc/ \
+               "$chroot_path"/lib/ \
+               "$chroot_path"/usr/ \
+               "$chroot_path"/var/ 
         ;;
     * ) ####################################################
-        mount --bind -o ro /bin/     "$chroot_path"/bin/
-        mount --bind -o ro /etc/     "$chroot_path"/etc/
-        mount --bind -o ro /lib/     "$chroot_path"/lib/
-        mount --bind -o ro /usr/     "$chroot_path"/usr/
-        mount --bind -o ro /sbin/    "$chroot_path"/sbin/
-        mount --bind -o ro /var/run/ "$chroot_path"/var/run/
+        mount --bind -o ro /bin/ "$chroot_path"/bin/
+        mount --bind -o ro /etc/ "$chroot_path"/etc/
+        mount --bind -o ro /lib/ "$chroot_path"/lib/
+        mount --bind -o ro /usr/ "$chroot_path"/usr/
+        mount --bind -o ro /var/ "$chroot_path"/var/
         ;;
 esac
 
