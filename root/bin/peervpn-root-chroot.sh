21c3253f044d5daa7531c31f55a56ca85580d355
Carlo Zappacosta <zappyk@gmail.com>
2017-06-21 10:11:30 +0200

    Miglioire...

--- root/bin/peervpn-root-chroot.sh
+++ root/bin/peervpn-root-chroot.sh
@@ -1,27 +1,20 @@
 #!/bin/env bash
 
-chroot_path='/var/empty/peervpn-1'
+chroot_path='/var/empty/peervpn-'$1
 
-[ ! -e "$chroot_path"/bin/ ] && mkdir -pv "$chroot_path"/bin/
-[ ! -e "$chroot_path"/etc/ ] && mkdir -pv "$chroot_path"/etc/
+[ ! -e "$chroot_path" ] && echo "peervpn chroot path \"$chroot_path\" not exist!" && exit 1
+
 [ ! -e "$chroot_path"/lib/ ] && mkdir -pv "$chroot_path"/lib/
 [ ! -e "$chroot_path"/usr/ ] && mkdir -pv "$chroot_path"/usr/
-[ ! -e "$chroot_path"/var/ ] && mkdir -pv "$chroot_path"/var/
 
 case $1 in
     -u) ####################################################
-        umount "$chroot_path"/bin/ \
-               "$chroot_path"/etc/ \
-               "$chroot_path"/lib/ \
-               "$chroot_path"/usr/ \
-               "$chroot_path"/var/ 
+        umount "$chroot_path"/lib/ \
+               "$chroot_path"/usr/
         ;;
     * ) ####################################################
-        mount --bind -o ro /bin/ "$chroot_path"/bin/
-        mount --bind -o ro /etc/ "$chroot_path"/etc/
         mount --bind -o ro /lib/ "$chroot_path"/lib/
         mount --bind -o ro /usr/ "$chroot_path"/usr/
-        mount --bind -o ro /var/ "$chroot_path"/var/
         ;;
 esac
 
