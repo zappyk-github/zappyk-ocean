45bf8be529b0bf7e7c1bd55ba2f666facc417966
Carlo Zappacosta <zappyk@gmail.com>
2017-08-05 17:34:00 +0200

    Miglioire...

--- root/bin/peervpn-root-chroot.sh
+++ root/bin/peervpn-root-chroot.sh
@@ -4,15 +4,21 @@ chroot_path='/var/empty/peervpn-'$1
 
 [ ! -e "$chroot_path" ] && echo "peervpn chroot path \"$chroot_path\" not exist!" && exit 1
 
+[ ! -e "$chroot_path"/bin/ ] && mkdir -pv "$chroot_path"/bin/
+[ ! -e "$chroot_path"/etc/ ] && mkdir -pv "$chroot_path"/etc/
 [ ! -e "$chroot_path"/lib/ ] && mkdir -pv "$chroot_path"/lib/
 [ ! -e "$chroot_path"/usr/ ] && mkdir -pv "$chroot_path"/usr/
 
 case $1 in
     -u) ####################################################
-        umount "$chroot_path"/lib/ \
+        umount "$chroot_path"/bin/ \
+               "$chroot_path"/etc/ \
+               "$chroot_path"/lib/ \
                "$chroot_path"/usr/
         ;;
     * ) ####################################################
+        mount --bind -o ro /bin/ "$chroot_path"/bin/
+        mount --bind -o ro /etc/ "$chroot_path"/etc/
         mount --bind -o ro /lib/ "$chroot_path"/lib/
         mount --bind -o ro /usr/ "$chroot_path"/usr/
         ;;
