c08ac46358f25646b325646dec350891e7efd897
Carlo Zappacosta <zappyk@gmail.com>
2017-08-10 19:11:12 +0200

    Miglioire...

--- root/bin/peervpn-exec.sh
+++ root/bin/peervpn-exec.sh
@@ -5,6 +5,8 @@ PEERVPN_run="/etc/peervpn/$PEERVPN_tag.conf"
 PEERVPN_log="/var/log/$PEERVPN_tag.log"
 PEERVPN_slp=3
 
+[ "$PEERVPN_tag" == "peervpn-1-client" ] && echo "Preparetion to bind mount chroot's diretcories..." && peervpn-root-chroot.sh 1
+
 echo -e "Start peervpn $PEERVPN_tag \c"
 
 nohup peervpn "$PEERVPN_run" >"$PEERVPN_log" 2>&1 &
