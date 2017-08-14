03d702d3e9e066b906195c127cb43f43d06216dc
Carlo Zappacosta <zappyk@gmail.com>
2017-08-14 16:55:47 +0200

    Miglioire...

--- home/zappyk/bin/scp-zappyk-rp1.incomings.sh
+++ home/zappyk/bin/scp-zappyk-rp1.incomings.sh
@@ -1,6 +1,6 @@
 #!/bin/env bash
 
-N_REMOTE=${1}; shift; [ -z "$N_REMOTE" ] && echo "$(basename "$0"): file remote?" && exit 1
+N_REMOTE=${1}; shift; [ -z "$N_REMOTE" ] && echo "$(basename "$0"): file/directory remote?" && exit 1
 D_LOCAL_=${1:-.}
 
 N_REMOTE=$(printf '%q' "$N_REMOTE")
@@ -9,6 +9,7 @@ D_REMOTE='/home/zappyk/Programmi/deluge/var/incomings'
 F_REMOTE="$H_REMOTE:$D_REMOTE/$N_REMOTE"
 
 SCP_OPTS=""
+SCP_OPTS="$SCP_OPTS -r"
 SCP_OPTS="$SCP_OPTS -l 100000" # 100000 kilobit => 12,5 Megabyte
 
 SCP_CMMD="scp $SCP_OPTS \"$F_REMOTE\" \"$D_LOCAL_\""
--- home/zappyk/bin/wget-zappyk-rp1.incomings.sh
+++ home/zappyk/bin/wget-zappyk-rp1.incomings.sh
@@ -1,7 +1,16 @@
 #!/bin/env bash
 
-N_REMOTE=${1}; shift; [ -z "$N_REMOTE" ] && echo "$(basename "$0"): file remote?" && exit 1
+N_REMOTE=${1}; shift; [ -z "$N_REMOTE" ] && echo "$(basename "$0"): file/directory remote?" && exit 1
 O_LOCAL_=${*}
+
+################################################################################
+# NOTE : if $N_REMOTE is a directory, don't forget char '/' at the end of name !
+lastchar=${N_REMOTE:$((${#N_REMOTE}-1)):1}
+if [ "$lastchar" != '/' ]; then
+    read -p "Remote \"$N_REMOTE\" is a directory? [s/N] " answer
+    [[ "$answer" =~ [sS] ]] && N_REMOTE="$N_REMOTE/"
+fi
+################################################################################
 
 #N_REMOTE=$(printf '%q' "$N_REMOTE")
 H_REMOTE='zappyk-rp1'
@@ -11,6 +20,11 @@ F_REMOTE="http://$H_REMOTE$D_REMOTE/$N_REMOTE"
 WGETOPTS=""
 WGETOPTS="$WGETOPTS --continue"
 WGETOPTS="$WGETOPTS --limit-rate=20m" # 20m => 20 Megabyte
+#-------= addiction options for directory
+WGETOPTS="$WGETOPTS --recursive --cut-dirs=1"
+WGETOPTS="$WGETOPTS --no-host-directories --no-parent"
+WGETOPTS="$WGETOPTS --execute robots=off"
+WGETOPTS="$WGETOPTS --reject \"index*\""
 
 WGETCMMD="wget $WGETOPTS \"$F_REMOTE\" $O_LOCAL_"
 
