65beec3b236ffdd7151dcfa17d36562bb96dcb50
Carlo Zappacosta <zappyk@gmail.com>
2017-08-18 00:23:19 +0200

    Miglioire...

--- home/zappyk/bin/wget-zappyk-rp1.incomings.sh
+++ home/zappyk/bin/wget-zappyk-rp1.incomings.sh
@@ -3,15 +3,6 @@
 N_REMOTE=${1} ; shift # [ -z "$N_REMOTE" ] && echo "$(basename "$0"): file/directory remote?" && exit 1
 O_LOCAL_=${*}
 
-################################################################################
-# NOTE : if $N_REMOTE is a directory, don't forget char '/' at the end of name !
-lastchar=${N_REMOTE:$((${#N_REMOTE}-1)):1}
-if [ "$lastchar" != '/' ]; then
-    read -p "Remote \"$N_REMOTE\" is a directory? [s/N] " answer
-    [[ "$answer" =~ [sS] ]] && N_REMOTE="$N_REMOTE/"
-fi
-################################################################################
-
 #N_REMOTE=$(printf '%q' "$N_REMOTE")
 H_REMOTE='zappyk-rp1'
 D_REMOTE='/incomings'
@@ -27,6 +18,20 @@ WGETOPTS="$WGETOPTS --execute robots=off"
 WGETOPTS="$WGETOPTS --reject \"index*\""
 
 WGETCMMD="wget $WGETOPTS \"$F_REMOTE\" $O_LOCAL_"
+
+################################################################################
+# NOTE : if $N_REMOTE is a directory, don't forget char '/' at the end of name !
+lastchar=${N_REMOTE:$((${#N_REMOTE}-1)):1}
+if [ "$lastchar" != '/' ]; then
+    if [ -n "$N_REMOTE" ]; then
+        read -p "Remote \"$N_REMOTE\" is a directory? [s/N] " answer
+        [[ "$answer" =~ [sS] ]] && N_REMOTE="$N_REMOTE/"
+    else
+        read -p "Download all the \"$F_REMOTE\" content? [S/n] " answer
+        [[ "$answer" =~ [nN] ]] && exit
+    fi
+fi
+################################################################################
 
 echo "$WGETCMMD"
 eval "$WGETCMMD"
