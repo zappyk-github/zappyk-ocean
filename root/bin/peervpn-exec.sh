930bbf7f56d9a99a7715e9bb5ee1fbbe51c072ad
Carlo Zappacosta <zappyk@gmail.com>
2017-08-19 17:12:35 +0200

    Miglioire...

--- root/bin/peervpn-exec.sh
+++ root/bin/peervpn-exec.sh
@@ -3,13 +3,11 @@
 PEERVPN_tag=$(basename "$0")
 PEERVPN_run="/etc/peervpn/$PEERVPN_tag.conf"
 PEERVPN_log="/var/log/$PEERVPN_tag.log"
-PEERVPN_slp=15
+PEERVPN_slp=20
 
-[ "$PEERVPN_tag" == "peervpn-1-client" ] && echo "Preparetion to bind mount chroot's diretcories..." && peervpn-root-chroot.sh 1
+[ "$PEERVPN_tag" == "peervpn-1-client" ] && echo "Preparetion to bind mount chroot's diretcories..." && peervpn-root-chroot.sh 1 | sed 's/^/\t/g'
 
-echo -e "Start peervpn $PEERVPN_tag \c"
-
-nohup peervpn "$PEERVPN_run" >"$PEERVPN_log" 2>&1 &
+echo -e "Start peervpn $PEERVPN_tag \c" && nohup peervpn "$PEERVPN_run" >"$PEERVPN_log" 2>&1 &
 
 for i in $(seq 1 $PEERVPN_slp); do echo -e ".\c"; sleep 1; done; echo
 
--- root/bin/peervpn-kill.sh
+++ root/bin/peervpn-kill.sh
@@ -1,28 +1,36 @@
 #!/bin/env bash
 
 THIS=$(basename "$0")
+THIS=${THIS/-kill/}
 
-OFF=${1:-false}
-TAG=${2:-peervpn}
+#TAG=${1:-peervpn}
+TAG=${1:-$THIS}
+OFF=${2:-false}
 PID=${3:-$$}
 ECN=0
 
 TAG_peervpn=$TAG
 PID_peervpn=$(pgrep -f "$TAG_peervpn" | grep -v $PID)
 
-first=true
+if [ -n "$PID_peervpn" ]; then
 
-for PID_find in $PID_peervpn; do
-    PSC_peervpn=$(ps -efj | grep "$PID_find " | grep -v grep)
-    if [ -n "$PSC_peervpn" ]; then
-	( $first ) && first=false && \
-        printf '#%.0s' {1..120} && echo
-        printf "# %5s = %s\n" "$PID_find" "$PSC_peervpn"
-        echo "kill $PID_find"
-        ECN=1
-    fi
-done
+    first=true
+    for PID_find in $PID_peervpn; do
+        PSC_peervpn=$(ps -efj | grep "$PID_find " | grep -v grep)
+        if [ -n "$PSC_peervpn" ]; then
+            ( $first ) && first=false && \
+            printf '#%.0s' {1..120} && echo
+            printf "# %5s = %s\n" "$PID_find" "$PSC_peervpn"
+            echo "kill $PID_find"
+            ECN=1
+        fi
+    done
+    [ $ECN -eq 0 ] && echo "Some peervpn is running (pid $PID_peervpn) but not $THIS!" && exit 1
 
 #CZ#( $OFF ) && killall "$TAG_peervpn"
+
+else
+    echo "No $THIS is running!" && exit 1
+fi
 
 exit $ECN
