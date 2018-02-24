2514ad8070eafd7e29dab9024a6e23b1ddb160eb
Carlo Zappacosta <zappyk@gmail.com>
2018-02-24 19:12:52 +0100

    Migliorie...

--- root/bin/dnf-update.sh
+++ root/bin/dnf-update.sh
@@ -3,9 +3,11 @@
 #dnf clean all
  dnf clean dbcache
  dnf clean expire-cache
- dnf -y update 'rpm*' 'dnf*'
-#dnf -y update --best --allowerasing
- dnf -y update
+
+#dnf -y update 'rpm*' 'dnf*'
+#dnf -y update
+ dnf -y update --best --allowerasing 'rpm*' 'dnf*'
+ dnf -y update --best --allowerasing
 
 #dnf distro-sync --setopt=deltarpm=0
 
