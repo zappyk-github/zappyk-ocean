995cee75671820c5fadf5046a7426346b6226c9f
Carlo Zappacosta <zappyk@gmail.com>
2021-11-09 11:16:39 +0100

    Migliorie programmi e aggiunte.

--- home/zappyk/bin/rpm-qa.sh
+++ home/zappyk/bin/rpm-qa.sh
@@ -7,6 +7,6 @@ packsp2='.'
 oformat="%-50{NAME}$packsp1%-25{VERSION}$packsp1%-25{RELEASE}$packsp2%{ARCH}\n"
 oformat="%-55{NAME}$packsp1%-30{VERSION}$packsp1%-30{RELEASE}$packsp2%{ARCH}\n"
 
-rpm -qa  --qf "$oformat" "*$package*" 2>&1 | tee ~/rpm-list.log
+rpm -qa  --qf "$oformat" "*$package*" 2>&1 | sort | tee ~/rpm-list.log
 
 exit
--- root/bin/check-update.sh
+++ root/bin/check-update.sh
@@ -4,15 +4,20 @@
 
  dnf-list.sh
 
- dnf check-update
+ dnf check-update ; rt=$? ; echo $rt
 
-#read -p '========== processo terminato, premi un pulsante per chiudere ==========' answare
- read -p '========== premi un pulsante per chiudere, "s" per aggiornare ==========' answare
-[ "$answare" == "s" ] && {
- dnf update
- echo    '========== processo terminato, premi un pulsante per chiudere'
- read -p '                               o "r" per ripetere il processo ==========' answare
- [ "$answare" == "r" ] && $0
-}
+if [ $rt -eq 0 ]; then
+    read -p '========== processo terminato, premi un pulsante per chiudere ==========' answare
+fi
+
+if [ $rt -eq 100 ]; then
+    read -p '========== premi un pulsante per chiudere, "s" per aggiornare ==========' answare
+    [ "$answare" == "s" ] && {
+        dnf -y update ; rt=$? ; echo $rt
+        echo    '========== processo terminato, premi un pulsante per chiudere'
+        read -p '                               o "r" per ripetere il processo ==========' answare
+        [ "$answare" == "r" ] && $0
+    }
+fi
 
 exit
